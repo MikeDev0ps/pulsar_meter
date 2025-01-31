6bc5517d21c5a09b585fbd3b5e5a95a97096a3d6
MikeDevOps <mikhail.dyuzhev@domrf.ru>
2025-01-31 10:54:24 +0300

    up

--- components/pulsar_m/__init__.py
+++ components/pulsar_m/__init__.py
@@ -1,15 +1,2 @@
-# __init__.py
 
-import esphome.config_validation as cv
-from esphome.components import sensor
-from esphome.core import CORE
-
-DEPENDENCIES = ["uart"]  # Указываем зависимости компонента
-
-def validate_config(config):
-    # Здесь можно добавить валидацию конфигурации
-    return config
-
-def to_code(config):
-    # Здесь можно добавить генерацию кода для ESPHome
-    pass
+DEPENDENCIES = ["uart"]
